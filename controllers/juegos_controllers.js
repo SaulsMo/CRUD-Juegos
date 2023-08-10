956ec1c0363deda5908348606a928fc824443bc1
SaulsMo <saul.moru@gmail.com>
2023-08-09 18:45:14 -0600

    ultimos detalles

---
+++ controllers/juegos_controllers.js
@@ -0,0 +1,58 @@
+const JuegosManager = require('../managers/juegos_managers')
+
+const juegosController = {
+    get:async(req, res) => {
+        try{
+            const juegos = await JuegosManager.get(req)
+            if(!juegos){
+                res.status(404).send ('Juego no encontrado')
+            }else{
+                res.status(200).send(juegos)
+            }
+        }catch (error){
+            res.status (500).send('Error en el servidor')
+        }
+    },
+    list:async(req, res) => {
+        try{
+            const juegosList = await JuegosManager.list()
+            res.status(200).send(juegosList)
+        }catch (error){
+            res.status(500).send('Error en el servidor')
+        }
+    },
+    create:async (req, res) => {
+        try{
+            const juegosCreated = await JuegosManager.creaet(req)
+            res.status(201).send('Juego Registrado')
+        }catch (error){
+            res.status(500).send('Error en el servidor')
+        }
+    },
+    update:async (req, res) => {
+        try{
+            const juegosUpdated = await JuegosManager.update(req)
+            if (!juegosUpdated) {
+                res.status(404).send('Juego no encontrado')
+            }else{
+                res.status(200).send('Juego actualizado')
+            }
+        }catch (error){
+            res.status(500).send('Error en el servidor')
+        }
+    },
+    destroy:async (req, res) => {
+        try{
+            const juegoDestroyed = await JuegosManager.destroy(req)
+            if(!juegoDestroyed){
+                res.status(404).send('Juego no encontrado')
+            }else{
+                res.status(200).send('Juego eliminado')
+            }
+        }catch(error){
+            res.status(500).send('Error en el servidor')
+        }
+    },
+}
+
+module.exports = juegosController
---
+++ managers/juegos_managers.js
@@ -0,0 +1,42 @@
+const juegoRepository = require('../Repository/juegos_repository')
+
+const JuegosManager = {
+    get:async (req, res) => {
+
+         const {id} = req.params
+         return await juegoRepository.findByPk(id)
+    },
+
+    list: async (req, res) => {
+
+        return await juegoRepository.findAll()
+    },
+
+    create:async(req, res) => {
+         return await juegoRepository.create({
+            juegosNom: req.body.juegosNom,
+            juegosDescription: req.body.juegosDescription,
+         })
+    },
+
+    updated: async (req, res) => {
+
+        const {id} = req.params
+        const juegos = await juegoRepository.findByPk(id)
+        if(juegos){
+            juegos.juegosNom = req.body.juegosNom
+            juegos.juegosDescription = req.body.juegosDescription
+            return await juegos.save()
+        }
+    },
+
+    destroy: async (req, res) => {
+        const {id} = req.params
+        const juegos = await juegoRepository.findByPk(id)
+        if (juegos){
+            return await juegos.destroy()
+        }
+    },
+}
+
+module.exports = JuegosManager
